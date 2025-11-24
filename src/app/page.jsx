42505cdba38067d6667cb6a60a31d9610b4f6f80
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2025-11-24 17:07:17 +0600

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
-import RecentList from "@/components/RecentList";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className=" bg-zinc-50 font-sans dark:bg-black">
-      <RecentList></RecentList>
+      <div>hello world</div>
     </div>
   );
 }
